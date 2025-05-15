96e4e30728abad2c630046e0a7bb57d10fa19214
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-15 15:07:33 +0900

    프로젝트 세팅 및 Swagger 설정

---
+++ dojooo/src/main/java/org/spring/dojooo/DojoooApplication.java
@@ -0,0 +1,13 @@
+package org.spring.dojooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoooApplication.class, args);
+	}
+
+}
---
+++ dojooo/src/main/java/org/spring/dojooo/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.spring.dojooo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        Info info = new Info()
+                .version("v1.0")
+                .title("Dojooo API")
+                .description("Dojooo API");
+        return new OpenAPI()
+                .info(info);
+    }
+}
