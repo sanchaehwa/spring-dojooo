3b83ead65f18b135b4eb498abc37e7267b2701db
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-17 20:56:43 +0900

    [Feat] API 응답 케이스 작성

---
+++ src/main/java/org/spring/dojooo/global/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package org.spring.dojooo.global.response;
+
+public class ApiResponse<T> {
+
+    private final T data; //API 응답의 실제 데이터를 저장할 필드 (변경 불가능하게 불변 객체로 설정)
+
+    public ApiResponse(T data) {
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(T data) {
+        return new ApiResponse<>(data);
+    }
+
+}
