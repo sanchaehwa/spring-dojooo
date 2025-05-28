3e0cf68eaaf58950debc19ec9a2df386c8cb5e99
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-28 20:23:21 +0900

    [Refactor] MemoRequest/Response → TechLogRequest/Response로 변경 및 리팩토링

--- src/main/java/org/spring/dojooo/main/contents/dto/TechLogRequest.java
+++ src/main/java/org/spring/dojooo/main/contents/dto/TechLogRequest.java
@@ -3,6 +3,10 @@ package org.spring.dojooo.main.contents.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.spring.dojooo.main.contents.domain.TechLog;
+import org.spring.dojooo.main.users.domain.User;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 //공통 요청 DTO
@@ -10,10 +14,25 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class MemoRequest {
+public class TechLogRequest {
 
-    private String nickname;
-    private String category;
-    private List<String> tags;
+    private Long userId;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+
+    TechLog toEntity(User user) {
+        return TechLog.builder()
+                .user(user)
+                .title(title)
+                .content(content)
+                .imageUrl(imageUrl)
+                .isRead(isRead)
+                .build();
+    }
+
+
 
 }
--- src/main/java/org/spring/dojooo/main/contents/dto/TechLogResponse.java
+++ src/main/java/org/spring/dojooo/main/contents/dto/TechLogResponse.java
@@ -1,18 +1,35 @@
 package org.spring.dojooo.main.contents.dto;
 
 import lombok.*;
+import org.spring.dojooo.main.contents.domain.TechLog;
+import org.spring.dojooo.main.users.domain.User;
+
 import java.time.LocalDateTime;
-import java.util.List;
 
 
 //공통 응답  DTO
 @Getter
 @Builder
-public abstract class MemoResponse {
+public class TechLogResponse {
 
-    private String nickname;
-    private String category;
-    private List<String> tags;
-    private LocalDateTime createdAt;
+   private String nickname;
+   private Long userId;
+   private String profileImage;
+   private String imageUrl;
+   private boolean isDeleted;
+   private boolean isRead;
+   private LocalDateTime createdAt;
+
+   public static TechLogResponse from(TechLog techLog, User user) {
+       return TechLogResponse.builder()
+               .nickname(user.getNickname())
+               .userId(user.getId())
+               .profileImage(user.getProfile().getProfileImage()!=null ? user.getProfile().getProfileImage() : null)
+               .imageUrl(techLog.getImageUrl())
+               .isDeleted(techLog.isDeleted())
+               .isRead(techLog.isRead())
+               .createdAt(techLog.getCreatedAt())
+               .build();
+   }
 
 }
