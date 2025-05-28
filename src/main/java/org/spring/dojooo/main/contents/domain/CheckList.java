4655b4bfeac66ad6e4930e09f85cb44c6a1e5d83
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-28 20:31:20 +0900

    [Feat] CheckList, TechLog 엔티티 추가

---
+++ src/main/java/org/spring/dojooo/main/contents/domain/CheckList.java
@@ -0,0 +1,50 @@
+package org.spring.dojooo.main.contents.domain;
+import jakarta.persistence.*;
+import lombok.*;
+import org.spring.dojooo.main.contents.model.TodoState;
+import org.spring.dojooo.main.users.domain.User;
+
+import java.util.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name="check_list")
+public class CheckList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="checklist_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+
+    @Column(nullable = false, length = 50)
+    private String task;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private TodoState todoState;
+
+    @Column
+    private Date schedule;
+
+    @Column
+    private boolean isDeleted;
+
+    @Builder
+    public CheckList(User user, String task, TodoState todoState, Date schedule, Boolean isDeleted) {
+        this.user = user;
+        this.task = task;
+        this.todoState = todoState != null ? todoState : TodoState.TODO;
+        this.isDeleted = isDeleted != null ? isDeleted : false;
+        this.schedule = new Date();
+
+    }
+
+
+}
